620832d16d64656abb5baece1f85f24c88b07354
matt <matthewk.rydalch@gmail.com>
2020-04-08 15:07:50 -0600

    Switched to base the driver off of mavsim_chap10 (path follower)

--- mpc/mavsim_mpc.py
+++ mpc/mavsim_mpc.py
@@ -8,7 +8,6 @@ import sys
 sys.path.append('..')
 import numpy as np
 import parameters.simulation_parameters as SIM
-import parameters.planner_parameters as PLAN
 
 from chap3.data_viewer import data_viewer
 from chap4.wind_simulation import wind_simulation
@@ -16,10 +15,10 @@ from chap6.autopilot import autopilot
 from chap7.mav_dynamics import mav_dynamics
 from chap8.observer import observer
 from chap10.path_follower import path_follower
-from chap11.path_manager import path_manager
-from chap11.waypoint_viewer import waypoint_viewer
+from chap10.path_viewer import path_viewer
 
-waypoint_view = waypoint_viewer()  # initialize the viewer
+# initialize the visualization
+path_view = path_viewer()  # initialize the viewer
 data_view = data_viewer()  # initialize view of data plots
 
 # initialize elements of the architecture
@@ -28,21 +27,14 @@ mav = mav_dynamics(SIM.ts_simulation)
 ctrl = autopilot(SIM.ts_simulation)
 obsv = observer(SIM.ts_simulation)
 path_follow = path_follower()
-path_manage = path_manager()
 
-# waypoint definition
-from message_types.msg_waypoints import msg_waypoints
-waypoints = msg_waypoints()
-waypoints.type = 'straight_line'
-waypoints.num_waypoints = 3
-Va = PLAN.Va0
-waypoints.ned = np.array([[0, 0, -100],
-                [1000, 0, -100],
-                [1000, 1000, -100]]).T
-waypoints.airspeed = np.array([[Va, Va, Va]])
-waypoints.course = np.array([[np.radians(0),
-                 np.radians(45),
-                 np.radians(-135)]]).T
+# path definition
+from message_types.msg_path import msg_path
+path = msg_path()
+path.flag = 'line'
+path.line_origin = np.array([[0.0, 0.0, -100.0]]).T
+path.line_direction = np.array([[0.5, 1.0, 0.0]]).T
+path.line_direction = path.line_direction / np.linalg.norm(path.line_direction)
 
 # initialize the simulation time
 sim_time = SIM.start_time
@@ -50,27 +42,26 @@ sim_time = SIM.start_time
 # main simulation loop
 print("Press Command-Q to exit...")
 while sim_time < SIM.end_time:
+
     #-------observer-------------
-    mav.update_sensors() #I added this, it seems to be necessary
+    mav.update_sensors() #I added, seems to be necessary
     measurements = mav._sensors  # get sensor measurements
     estimated_state = obsv.update(measurements)  # estimate states from measurements
 
-    #-------path manager-------------
-    path = path_manage.update(waypoints, PLAN.R_min, estimated_state)
-
     #-------path follower-------------
     autopilot_commands = path_follow.update(path, estimated_state)
+    # autopilot_commands = path_follow.update(path, mav.msg_true_state) #for debugging
 
     #-------controller-------------
     delta, commanded_state = ctrl.update(autopilot_commands, estimated_state)
 
     #-------physical system-------------
-    # current_wind = wind.update()  # get the new wind vector
-    current_wind = np.zeros((6,1))
+    current_wind = wind.update()  # get the new wind vector
+    # current_wind = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
     mav.update_state(delta, current_wind)  # propagate the MAV dynamics
 
     #-------update viewer-------------
-    waypoint_view.update(waypoints, path, mav.msg_true_state)  # plot path and MAV
+    path_view.update(path, mav.msg_true_state)  # plot path and MAV
     data_view.update(mav.msg_true_state, # true states
                      estimated_state, # estimated states
                      commanded_state, # commanded states
@@ -78,7 +69,3 @@ while sim_time < SIM.end_time:
 
     #-------increment time-------------
     sim_time += SIM.ts_simulation
-
-
-
-
